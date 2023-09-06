76e441cc5a1b26a8b04ed85c893c024ed034e7f7
rzeraat.tur <rzeraat.tur@gmail.com>
2023-09-07 01:18:09 +0300

    connect to elequont sql

--- api/fetchPrice.js
+++ api/fetchPrice.js
@@ -1,7 +1,13 @@
 const axios = require("axios");
+const { Pool } = require("pg");
 
-const GITHUB_TOKEN = "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"; // Replace with your token
-const GIST_ID = "dcb17ae937b79386d092cf7f8d6c31b6#file-bitcoin_prices-csv"; // Replace with your Gist ID once you create it
+// ElephantSQL connection URL
+const connectionString =
+    "postgres://fchszuxs:hKdwCjO_2u3ogekA_LvMShLA2uUnIIrt@berry.db.elephantsql.com/fchszuxs";
+
+const pool = new Pool({
+    connectionString: connectionString,
+});
 
 module.exports = async(req, res) => {
     try {
@@ -11,40 +17,12 @@ module.exports = async(req, res) => {
         const price = response.data.result.price;
         const time = new Date().toISOString();
 
-        const record = `${time}, ${price}\n`;
-
-        // Fetch the current content of the Gist
-        const gistResponse = await axios.get(
-            `https://api.github.com/gists/${GIST_ID}`, {
-                headers: {
-                    Authorization: `token ${GITHUB_TOKEN}`,
-                },
-            },
-        );
-        const currentContent =
-            gistResponse.data.files["bitcoin_prices.csv"].content;
-
-        // Append the new record
-        const updatedContent = currentContent + record;
-
-        // Update the Gist with the new content
-        await axios.patch(
-            `https://api.github.com/gists/${GIST_ID}`, {
-                files: {
-                    "bitcoin_prices.csv": {
-                        content: updatedContent,
-                    },
-                },
-            }, {
-                headers: {
-                    Authorization: `token ${GITHUB_TOKEN}`,
-                },
-            },
-        );
+        // Insert the data into the PostgreSQL database
+        const query = "INSERT INTO bitcoin_prices(time, price) VALUES($1, $2)";
+        await pool.query(query, [time, price]);
 
         res.status(200).send(`Price saved: ${price}`);
     } catch (error) {
-        console.error("Error:", error);
         res.status(500).send(error);
     }
 };
